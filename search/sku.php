905066f046bb2a568b283d9e721c186de10e8c1e
jogind3r <jogind3r@gmail.com>
2015-07-02 16:57:36 +0530

    structure

--- search/sku.php
+++ search/sku.php
@@ -5,13 +5,13 @@ require('../db.php');
 
 if (isset($_GET['term'])){
 	$return_arr = array();
-
+	//echo "==".$_GET['term']."==";
 	try {
 	    $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	    $stmt = $conn->prepare('SELECT distinct sku FROM stockitemtypes WHERE type=:term');
-	    $stmt->execute(array('term' => '%'.$_GET['term'].'%'));
+	//echo "SELECT distinct SKU FROM stockitemtypes WHERE type='$_GET[term]'";
+	    $stmt = $conn->prepare("SELECT distinct SKU FROM stockitemtypes WHERE type='$_GET[term]'");
+	    $stmt->execute();
 
 	    while($row = $stmt->fetch()) {
 	        $return_arr[] =  $row;
